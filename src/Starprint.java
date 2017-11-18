653a69b8cd5586b94d842fdc083c5b9740919a4e
vinayhce009 <vinayhce009@gmail.com>
2017-11-18 16:19:27 +0530

    commit

---
+++ src/Starprint.java
@@ -0,0 +1,18 @@
+
+public class Starprint {
+	public static void main(String[] args) 
+	{
+	int i,j;
+	for (i=5;i>=5;i-- )
+	{
+		for (j=5;j<=i;j-- )
+		{
+      System.out.print("*");
+		}
+   System.out.println();
+	}
+	}
+
+}
+
+
